16b2ff37cf8e1e9fece08b3600c11ed630ebda05
Simanta Patowary <simanta25@Simantas-MacBook-Air.local>
2026-06-28 22:47:44 +0530

    add movies

---
+++ app/components/MovieCard.tsx
@@ -0,0 +1,31 @@
+type Props = {
+  title: string;
+  genre: string;
+  image: string;
+};
+
+const MovieCard = ({
+  title,
+  genre,
+  image,
+}: Props) => {
+  return (
+    <div className="w-60 flex-shrink-0">
+      <img
+        src={image}
+        alt={title}
+        className="h-[380px] w-full rounded-xl object-cover"
+      />
+
+      <h3 className="mt-3 text-lg font-semibold line-clamp-2">
+        {title}
+      </h3>
+
+      <p className="text-gray-500 text-md line-clamp-2">
+        {genre}
+      </p>
+    </div>
+  );
+};
+
+export default MovieCard;
