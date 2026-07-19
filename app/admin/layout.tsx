ae2a9d00d9018cb5e6130d71cf54d767731f6a51
seriouspatowary <simantapatowary963@gmail.com>
2026-07-19 11:49:09 +0530

    fix admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -3,36 +3,44 @@
 import { useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import AuthInitializer from "../AuthInitializer";
+
+
 
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
   const router = useRouter();
 
   const { user, isAuthenticated } = useSelector(
-    (state: any) => state.auth
+    (state:any)=>state.auth
   );
 
-  useEffect(() => {
-    if (!isAuthenticated) {
+
+  useEffect(()=>{
+
+    if(!isAuthenticated){
       router.replace("/login");
       return;
     }
 
-    if (user?.role !== "administrator") {
+    if(user?.role !== "administrator"){
       router.replace("/");
     }
-  }, [user, isAuthenticated, router]);
 
-  if (!isAuthenticated || user?.role !== "administrator") {
+  },[user,isAuthenticated,router]);
+
+
+  if(!isAuthenticated || user?.role !== "administrator"){
     return null;
   }
 
-  return <>
-   <AuthInitializer />
-    {children}
-  </>;
+
+  return (
+    <>
+      {children}
+    </>
+  );
 }
